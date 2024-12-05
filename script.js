abbfaec478aca713526f3e583c2b3b0c0b71d95a
franckwilfried <wilfiedfranck@gmail.com>
2024-12-05 03:20:18 +0100

    05/12/2024 A

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
       alert("Please fill in all fields.");
     }
   });
-  
+
+/////////////////////////////////
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", DisplayContent);
+DisplayContent();
+
+function DisplayContent() {
+  const TriggerBottom = (window.innerHeight/5)*4;
+
+  boxes.forEach((box) => {
+    const topBox = box.getBoundingClientRect().top;
+
+    if (topBox < TriggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+////////////////////////////////
